5d9ff2bdb4014badc6399ad89aec6cae565810d5
WhatDoes-ThisDo <86938679+WhatDoes-ThisDo@users.noreply.github.com>
2021-07-09 23:04:04 -0400

    Update Modules.py
    Added simple character class. Need to manage organization of scenario nuances.

--- Modules.py
+++ Modules.py
@@ -3,14 +3,38 @@ class Story():
     def __init__(self, friend, foe):
         self.friend = friend
         self.foe = foe
-        self.scenario = (random.randint(3,4) * random.randint(1,2)) % 2
+        self.scenario = (random.randint(3,4) * 12345 * random.randint(1,2)) % 2
+        self.scenario += ((random.randint(3,4) * 12345 * random.randint(1,2)) % 2)
         if self.scenario == 0:
             self.character = friend
         else:
             self.character = foe
 
-newStory = Story("bob","janice")
-print(newStory.foe)
-print(newStory.friend)
-print(newStory.scenario)
-print(newStory.character)
+##newStory = Story("bob","janice")
+##print(newStory.foe)
+##print(newStory.friend)
+##print(newStory.scenario)
+##print(newStory.character)
+
+#ToDo:
+#  
+
+class Character():
+    def __init__(self, name, alignment, aggro_range, resolve_range,
+                 questions, answers):
+        self.name = name
+        self.alignment = alignment
+        self.aggro_range = aggro_range
+        self.resolve_range = resolve_range
+        self.questions = questions
+        self.answers = answers
+        
+    def __repr__(self):
+        return self.name
+
+bastian = Character('Bastian','baaayad', 5,5,'wut?','Maybe')
+
+print(bastian)
+        
+        
+        
