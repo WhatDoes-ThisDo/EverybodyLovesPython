9e6b744ceab3eb8952c76af75a8f2c997c51d4e9
WhatDoes-ThisDo <86938679+WhatDoes-ThisDo@users.noreply.github.com>
2021-07-10 23:38:00 -0400

    Update Modules.py
    Simplified the Character class to include the q&a as a dictionary

--- Modules.py
+++ Modules.py
@@ -20,21 +20,29 @@ class Story():
 #  
 
 class Character():
-    def __init__(self, name, alignment, aggro_range, resolve_range,
-                 questions, answers):
+    def __init__(self, name, friend,
+                 q_and_a):
         self.name = name
-        self.alignment = alignment
-        self.aggro_range = aggro_range
-        self.resolve_range = resolve_range
-        self.questions = questions
-        self.answers = answers
+        self.friend = friend
+        self.q_and_a = q_and_a #question being q_and_a[num][0] and answer being q_and_a[num][1]
+        # limit the number of allowed questions to half the total
+        self.answer_countdown = round(len(self.q_and_a.keys())/2)
         
     def __repr__(self):
         return self.name
+    def get_question(self, numKey):
+        return self.q_and_a[numKey][0]
+    def get_answer(self, numKey):
+        return self.q_and_a[numKey][1]
+    
 
-bastian = Character('Bastian','baaayad', 5,5,'wut?','Maybe')
+#test the class using an instance
+bastian = Character('Big Friendly Python',False, {1:["What is your name?","Big Friendly Python Junior Esquire 3rd"], 2:["What is your quest?","To do friendly stuff and help you in this game. :)"],3:["What is your favorite color?","Blue-No! Wait!"], 4:["Is this a question?","Yes, it is indeed"],5:["Is this also a question?","Yep......"], 6:["I like turtles.","What?"]})
 
 print(bastian)
+print(bastian.answer_limit)
+print(bastian.get_question(1))
+print(bastian.get_answer(1))
         
         
         
