4ae0f2ef4df5678c5cb84b3bd5101434a432d625
WhatDoes-ThisDo <86938679+WhatDoes-ThisDo@users.noreply.github.com>
2021-08-07 12:02:39 -0400

    Add files via upload
    Added an in-progress alphabet template and sample code for test.

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import Modules
+
+bastian = Modules.Character('Big Friendly Python Junior Esquire 3rd',False, {1:["What is your name?","Big Friendly Python Junior Esquire 3rd"], 2:["What is your quest?","To do friendly stuff and help you in this game. :)"],3:["What is your favorite color?","Blue-No! Wait!"], 4:["Is this a question?","Yes, it is indeed"],5:["Is this also a question?","Yep......"], 6:["I like turtles.","What?"]})
+print(bastian.get_question(1))
+##print(bastian)
+##print(bastian.friend)
+##
+##def main():
+##
+##    #Display main menu
+##
+##    #Prompt user to press space to continue
+##
+##    #Ask for the user's name
+##
+##    #Load Character and story from binaries
+##
+##main()
