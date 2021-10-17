dfd5f7f49eb9400f119d338510d6ae63f80aa151
WhatDoes-ThisDo <86938679+WhatDoes-ThisDo@users.noreply.github.com>
2021-10-17 17:43:38 -0400

    Update main.py
    Adjusted character creation format to recent changes

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 import sys
 import Modules
 
-bastian = Modules.Character('Big Friendly Python Junior Esquire 3rd',False, {1:["What is your name?","Big Friendly Python Junior Esquire 3rd"], 2:["What is your quest?","To do friendly stuff and help you in this game. :)"],3:["What is your favorite color?","Blue-No! Wait!"], 4:["Is this a question?","Yes, it is indeed"],5:["Is this also a question?","Yep......"], 6:["I like turtles.","What?"]})
-print(bastian.get_question(1))
-##print(bastian)
-##print(bastian.friend)
+bastian = Modules.Character('Big Friendly Python'
+,False
+, {'win': "You run away right before the 'friendly' python bytes you. You win, I guess...", 'lose':'You were swallowed by the snake....'}
+, {'win': 'You just received a new friend! With big snake hugs! The python protects you from the jaguar onslaught', 'lose': 'You run away and trip over a branch... you were eaten by pirhanas'}
+, {1:["What is your name?","Big Friendly Python Junior Esquire 3rd"], 2:["What is your quest?","To do friendly stuff and help you in this game."],3:["What is your favorite color?","Blue-No! Wait!"], 4:["Is this a question?","Yes, it is indeed"],5:["Is this also a question?","Yep......"], 6:["I like turtles.","What?"]})
+#print(bastian.get_question(1))
+#print(bastian)
+#print(bastian.friend)
 ##
 ##def main():
 ##
