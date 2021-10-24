34945776e4275b8bf046a5a96a88ae5177628c54
WhatDoes-ThisDo <86938679+WhatDoes-ThisDo@users.noreply.github.com>
2021-10-23 22:38:07 -0400

    Update main.py
    Include the start screen animation and formatted character introduction.

--- main.py
+++ main.py
@@ -2,21 +2,42 @@ import os
 import sys
 import Modules
 import MenuArt
+import time
 
-print(MenuArt.menuArt())
+divider = '------------------------------------------------------------'
+
+# start screen animation
+print(MenuArt.menuArt1())
+time.sleep(0.5)
+print(MenuArt.menuArt2())
+time.sleep(0.5)
+print(MenuArt.menuArt3())
+time.sleep(0.5)
+print(MenuArt.menuArt4())
+time.sleep(0.5)
+print(MenuArt.menuArt3())
+time.sleep(0.5)
+print(MenuArt.menuArt2())
+time.sleep(0.5)
+print(MenuArt.menuArt1())
 print("Want to Start?")
 startOrQuit = str(input('Press "Y" to Begin or Other Keys to Quit: '))
 if startOrQuit.upper() == "Y":
     bastian = Modules.Character('Big Friendly Python', '''For some reason, you are hiking in a jungle and hear
-"Hi! I am a model character to test the game\'s story system! I am also a friendly python thing!
+"Hi! I am a model character to test the game\'s story system!
+I am also a friendly python thing!
 It\'s not safe here, so you\'ll have to come with me."'''
 , {'win': "You run away right before the 'friendly' python bites you. You win, I guess...", 'lose':'You were swallowed by the snake....'}
 , {'win': 'You just received a new friend! With big snake hugs! The python protects you from the jaguar onslaught', 'lose': 'You run away and trip over a branch... you were eaten by pirhanas'}
 , {1:["What is your name?","Big Friendly Python Junior Esquire 3rd"], 2:["What is your quest?","To do friendly stuff and help you in this game."],3:["What is your favorite color?","Blue-No! Wait!"], 4:["Is this a question?","Yes, it is indeed"],5:["Is this also a question?","Yep......"], 6:["I like turtles.","What?"]})
-    print(bastian.get_question(1))
+##    print(bastian.get_question(1))
     story = Modules.Story(bastian)
-    print(bastian.friend)
-    print(bastian.get_intro())
+##    print(bastian.friend)
+    print(divider)
+    input('\n\n' + bastian.get_intro() + '\n\n' + divider + '\nPress Enter to Continue')
+    print(divider)
+    print('\n\n' + """You might want to ask this character a few questions first.
+You just met them, after all.""" + '\n\n' + divider)
 else:
     sys.exit("Have a good one!")
 #print(bastian.get_question(1))
