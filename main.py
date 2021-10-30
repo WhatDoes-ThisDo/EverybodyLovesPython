f3c0fc0862e2ad27cd4ff223a9ce5c32676001d8
WhatDoes-ThisDo <86938679+WhatDoes-ThisDo@users.noreply.github.com>
2021-10-30 13:02:36 -0400

    Update main.py
    Condensed the animation to a loop and forced the program to clear screen for every transmission.

--- main.py
+++ main.py
@@ -4,25 +4,32 @@ import Modules
 import MenuArt
 import time
 
+def clearScreen():
+    if os.name.lower() == "posix":
+        os.system("clear")
+    else:
+        os.system("cls")
+
 divider = '------------------------------------------------------------'
 
 # start screen animation
-print(MenuArt.menuArt1())
-time.sleep(0.5)
-print(MenuArt.menuArt2())
-time.sleep(0.5)
-print(MenuArt.menuArt3())
-time.sleep(0.5)
-print(MenuArt.menuArt4())
-time.sleep(0.5)
-print(MenuArt.menuArt3())
-time.sleep(0.5)
-print(MenuArt.menuArt2())
-time.sleep(0.5)
-print(MenuArt.menuArt1())
+animation = [MenuArt.menuArt1(), MenuArt.menuArt2(), MenuArt.menuArt3(), MenuArt.menuArt4()]
+for frame in animation:
+    time.sleep(0.5)
+    clearScreen()
+    print(frame)
+for frame in animation[-2::-1]:
+    time.sleep(0.5)
+    clearScreen()
+    print(frame)
+
+#prompt user to start
 print("Want to Start?")
 startOrQuit = str(input('Press "Y" to Begin or Other Keys to Quit: '))
 if startOrQuit.upper() == "Y":
+
+#   create character object
+    clearScreen()
     bastian = Modules.Character('Big Friendly Python', '''For some reason, you are hiking in a jungle and hear
 "Hi! I am a model character to test the game\'s story system!
 I am also a friendly python thing!
@@ -30,28 +37,20 @@ It\'s not safe here, so you\'ll have to come with me."'''
 , {'win': "You run away right before the 'friendly' python bites you. You win, I guess...", 'lose':'You were swallowed by the snake....'}
 , {'win': 'You just received a new friend! With big snake hugs! The python protects you from the jaguar onslaught', 'lose': 'You run away and trip over a branch... you were eaten by pirhanas'}
 , {1:["What is your name?","Big Friendly Python Junior Esquire 3rd"], 2:["What is your quest?","To do friendly stuff and help you in this game."],3:["What is your favorite color?","Blue-No! Wait!"], 4:["Is this a question?","Yes, it is indeed"],5:["Is this also a question?","Yep......"], 6:["I like turtles.","What?"]})
-##    print(bastian.get_question(1))
+
     story = Modules.Story(bastian)
-##    print(bastian.friend)
+##  Create introduction and grab initial question as input
     print(divider)
     input('\n\n' + bastian.get_intro() + '\n\n' + divider + '\nPress Enter to Continue')
+    clearScreen()
     print(divider)
+    questionNum = int(len(bastian.q_and_a) / 2)
     print('\n\n' + """You might want to ask this character a few questions first.
 You just met them, after all.""" + '\n\n' + divider)
+    print("You only have time to ask " + str(questionNum) + " questions. Ask wisely!")
+    for item in bastian.q_and_a.keys():
+        print(str(item) + '. ' + bastian.get_question(item) + '\n')
+    print(divider) 
+    theQuestion = input("Choose the number key for the question you want to ask, then press Enter: ")
 else:
     sys.exit("Have a good one!")
-#print(bastian.get_question(1))
-#print(bastian)
-#print(bastian.friend)
-##
-##def main():
-##
-##    #Display main menu
-##
-##    #Prompt user to press space to continue
-##
-##    #Ask for the user's name
-##
-##    #Load Character and story from binaries
-##
-##main()
